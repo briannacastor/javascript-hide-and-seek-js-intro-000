63948ab6dd49ce7e7593657d9ebd727bbd9230b6
capable-optimization-2034 <brianna.l.castor@gmail.com>
2020-10-13 04:05:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function increaseRankBy(n) {
     }
 
 }
+function increaseRankBy(n) {
+  var ranks = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+  }
+}
 
 function deepestChild(){
   var deep = document.getElementById('grand-node').querySelectorAll('div')
